69dd803ed5819d4c0e66cf1df946b54157c334fa
SCaicedo99 <43104210+SCaicedo99@users.noreply.github.com>
2018-12-26 22:47:18 -0500

    Practiced with npm and lodash
    Used require to export own functions from different files, and third party functions from npm

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,11 @@
 console.log('Starting app.js');
-
-const fs = require('fs'); // This imports all the functions in fs
-const os = require('os'); // Importing OS module
-const notes = require('./notes.js');
-
-var res = notes.add(5,-2);
-console.log(res);
+//
+// const fs = require('fs'); // This imports all the functions in fs
+// const os = require('os'); // Importing OS module
+// const notes = require('./notes.js');
+//
+// var res = notes.add(5,-2);
+// console.log(res);
 
 // var user = os.userInfo();
 
@@ -16,3 +16,15 @@ console.log(res);
 //     console.log('Unable to write to file');
 //   }
 // });
+
+
+// Using npm files here:
+
+const _ = require('lodash');
+
+var filteredArray = _.uniq(['Sebastian', 1, 2, 'Sebastian', 'sebastian']); // This uniq function returns the filteredArray with no 
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString("Sebastian"));
+// console.log(_.isString(2));
