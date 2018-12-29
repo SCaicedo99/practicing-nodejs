cb1bb649443143de14b683c9094203ffa10d1e5e
SCaicedo99 <43104210+SCaicedo99@users.noreply.github.com>
2018-12-29 13:06:55 -0500

    Used yargs
    Used yargs to use user input more efficiently

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,6 +1,28 @@
 console.log('Starting app.js');
 
+// TODO when initially runnning this script make sure to install all of the packages needed with the command line
+// npm install
+
 const fs = require('fs'); // This imports all the functions in fs
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
+
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read'){
+  notes.read(argv.title);
+} else if (command === 'remove'){
+  notes.remove(argv.title);
+} else {
+  console.log('Command not recognized');
+};
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote =  () => {
-  console.log('AddNote');
-  return 'NewNote';
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var read = (title) => {
+  console.log('Reading note with title: ', title);
+};
+
+var remove = (title) => {
+  console.log('Removing the note with title: ', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  read,
+  remove
 };
